37b746bac4d56c2f406d2198c7e778dcda2fd87a
Joshua <joshkiro.john212@gmail.com>
2026-04-29 10:21:57 +0000

    feature:Add dark mode support

--- App.tsx
+++ App.tsx
@@ -4,22 +4,24 @@ import { View, StyleSheet } from 'react-native';
 import './src/i18n';
 import OfflineIndicator from './src/components/OfflineIndicator';
 import { useSplashGuard } from './src/components/SplashGuard';
-import AppNavigator from './src/navigation';
 import { PetProvider } from './src/context/PetContext';
+import AppNavigator from './src/navigation';
+import { ThemeProvider } from './src/utils/useTheme';
 
 export default function App() {
   const { appReady } = useSplashGuard();
 
-  // Render nothing (splash is still visible) until critical init is done
   if (!appReady) return <View style={styles.root} />;
 
   return (
-    <PetProvider>
-      <View style={styles.root}>
-        <OfflineIndicator />
-        <AppNavigator />
-      </View>
-    </PetProvider>
+    <ThemeProvider>
+      <PetProvider>
+        <View style={styles.root}>
+          <OfflineIndicator />
+          <AppNavigator />
+        </View>
+      </PetProvider>
+    </ThemeProvider>
   );
 }
 
